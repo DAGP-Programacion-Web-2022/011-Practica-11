a5591d6d32a7c8eb4e0ed741b16a3b3550b0e22f
dagp-ucol <dgentner@ucol.mx>
2022-02-19 21:09:17 -0600

    added logGreeting1

--- functions/app.js
+++ functions/app.js
@@ -7,6 +7,11 @@ logGreeting(function () {
     conesole.log(`Hello from a function created on the fly`)
 }); //Function 02
 
+// Interpolación de Strings Clásica
+let logGreeting1 = (miNombre, miColor) => {
+    conesole.log("Hola " + miNombre + " buenos dias! Tu color favorito es el " + miColor);
+}
+
 let greetMe = () => console.log('Hello from de function expression');
 greetMe(); //Function 03
 
