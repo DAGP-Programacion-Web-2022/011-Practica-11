b5e4edf79b654eb44aaed0d8bf896d73c76fc94c
dagp-ucol <dgentner@ucol.mx>
2022-02-19 21:35:02 -0600

    persona object with getName function inside

--- objects/app.js
+++ objects/app.js
@@ -1,1 +1,9 @@
-let obj = {};
+let persona = {
+    nombre: "Carlos",
+    apellido: "Ulibarri",
+    getName: function () {
+        return `${this.nombre} ${this.apellido}`
+    }
+}
+
+console.log(persona.getName());
