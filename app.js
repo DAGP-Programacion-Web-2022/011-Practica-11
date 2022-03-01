7fc84ff0b310fd4d08bc4562ffbdb98e5a12bc69
dagp-ucol <dgentner@ucol.mx>
2022-03-01 08:56:42 -0600

    finished classroom code

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let obj = {
     nombre: "Carlos",
     edad: 45,
     apellido: "Ulibarri",
-    mouseOver: function() {
-        console.log("Mouse Over")
+    keyPress: function() {
+        console.log("keyPress")
     },
     onClick: function() {
         console.log("On Clcik")
@@ -14,6 +14,21 @@ console.log(obj.nombre);
 console.log(obj["nombre"]);
 
 let myKey = "nombre";
-myKey(obj[myKey]);
+console.log((obj[myKey]));
 myKey = "keyPress";
-obj[myKey()];
+obj[myKey]();
+
+let myArray = [];
+//myArray.push(8);
+//myArray.push("Hola, como estas?");
+//myArray.push(function(){console.log("Hola desde function")});
+//myArray.push(obj);
+//console.log(myArray);
+
+myArray.push(()=>console.log("F1"));
+myArray.push(()=>console.log("F2"));
+myArray.push(()=>console.log("F3"));
+
+myArray.forEach(element => {
+   element(); 
+});
