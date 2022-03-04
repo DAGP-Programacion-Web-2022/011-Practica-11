529c0447412fadd6c086635ef9a963ae8861bef3
dagp-ucol <dgentner@ucol.mx>
2022-03-03 20:26:32 -0600

    first changes to code to create the practice

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
+// Object Initialiazed
 let obj = {
     nombre: "Carlos",
     edad: 45,
     apellido: "Ulibarri",
-    keyPress: function() {
-        console.log("keyPress")
-    },
-    onClick: function() {
-        console.log("On Clcik")
-    }
 }
 
+// Accesing object with keys and Array with string
 console.log(obj.nombre);
 console.log(obj["nombre"]);
 
-let myKey = "nombre";
-console.log((obj[myKey]));
-myKey = "keyPress";
-obj[myKey]();
 
-let myArray = [];
-//myArray.push(8);
-//myArray.push("Hola, como estas?");
-//myArray.push(function(){console.log("Hola desde function")});
-//myArray.push(obj);
-//console.log(myArray);
 
-myArray.push(()=>console.log("F1"));
-myArray.push(()=>console.log("F2"));
-myArray.push(()=>console.log("F3"));
 
-myArray.forEach(element => {
-   element(); 
-});
+
+
+
+// let myKey = "nombre";
+// console.log((obj[myKey]));
+// myKey = "keyPress";
+// obj[myKey]();
+
+// let myArray = [];
+// //myArray.push(8);
+// //myArray.push("Hola, como estas?");
+// //myArray.push(function(){console.log("Hola desde function")});
+// //myArray.push(obj);
+// //console.log(myArray);
+
+// myArray.push(()=>console.log("F1"));
+// myArray.push(()=>console.log("F2"));
+// myArray.push(()=>console.log("F3"));
+
+// myArray.forEach(element => {
+//    element(); 
+// });
+
+// keyPress: function() {
+//     console.log("keyPress")
+// },
+// onClick: function() {
+//     console.log("On Clcik")
+// }
