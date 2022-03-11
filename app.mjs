28d8c93606afb7fcd7b3f2c37465b744029d9b59
dagp-ucol <dgentner@ucol.mx>
2022-03-10 22:49:39 -0600

    Fixed new nodeJS syntax and all working successfully

--- app.mjs
+++ app.mjs
@@ -1,9 +1,9 @@
 // Modules imported
 import events from './config.mjs'
-import Emitter from './emisor/emitter.mjs'
+import EventEmitter from 'events';
 
 // Initialized Emitter in a new variable.
-var emtr = new Emitter();
+var emtr = new EventEmitter();
 
 // Testing Emitter functions / ON functions allows to add properties to that variable initialized
 emtr.on(events.GREET, () => {
@@ -27,12 +27,6 @@ emtr.on(events.JUMP, () => {
 // Testing Emitter functions / EMIT functions allows to show properties to that variable initialized
 console.log(emtr);
 emtr.emit('jump');
-
-
-
-
-
-
 
 
 
