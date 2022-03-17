73f43544be5b1dad177f06033db111600dd95736
dagp-ucol <dgentner@ucol.mx>
2022-03-16 22:20:29 -0600

    ✅ Added documentation and finished practica 09 development

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
+// Express Node Module is imported successfully.
 import express from 'express';
+// Declaration of app constant and express function is assigned to app const.
 const app = new express();
 
+// Sending information to the server and route assignation.
 app.get('/', (req, res) => {
     res.send('Hello World, this is the root route.')
 });
 
+// Listen() function assigns the local host port that is being used by express.
 app.listen(3000);
+
+// Sending information to the server and route assignation.
 app.get('/uno', (req, res) => {
     res.send('Hello World, this is the uno route.')
 });
 
+// Sending information to the server and route assignation.
 app.get('/prueba', (req, res) => {
     res.send('Hello World, this is the prueba route.')
 });
