895da04de4d13115d60dce506d0504a50f75f2c2
dagp-ucol <dgentner@ucol.mx>
2022-03-16 23:28:23 -0600

    ✅ Practica 11 Finished and added documentation

--- src/app.js
+++ src/app.js
@@ -6,15 +6,16 @@ const app = new express();
 // This variable assigns the port that express will be using.
 let port = process.env.PORT || 3000;
 
-
+// This function create and assign virtual directory that points to a static directory.
 app.use('/assets', express.static('./public'));
 
-app.use('/', (req, res) => {
+// This function creates a debug log in console of any page that is requested.
+app.use('/', (req, res, next) => {
     console.log(`Request Url ${req.url}`);
     next();
 });
 
-// Sending information to the server and route assignation (HTML).
+// This function sends information to the server and route assignation (HTML).
 app.get('/', (req, res) => {
     res.send(`<!DOCTYPE html>
     <head>
@@ -24,14 +25,20 @@ app.get('/', (req, res) => {
     </html>`);
 });
 
-// Sending information to the server and route assignation (JSON).
+// This function sends information to the server and route assignation (JSON).
 app.get('/api', (req, res) => {
     res.json({ firstname: 'John', lastname: 'Doe'});
 });
 
-// Sending information to the server and route assignation (HTML with REQUEST INFO (HTTP ROUTE)).
+// This function sends information to the server and route assignation (HTML with REQUEST INFO (HTTP ROUTE)).
 app.get('/person/:id', (req, res) => {
-    res.send(`<!DOCTYPE html><head></head><body><h1>Persona solicitada: ${req.params.id} </h1></body></html>`);
+    res.send(`<!DOCTYPE html>
+    <head>
+    <link rel="stylesheet" href="assets/style.css">
+    </head>
+    <body>Persona solicitada: ${req.params.id}
+    </body>
+    </html>`);
 });
 
 // This function assigns the port that express will be using, port parameter was assigned in port variable.
