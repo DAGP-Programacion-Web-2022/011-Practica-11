48ac0db9dab9c652b75897d9b222580592c9c1a6
dagp-ucol <dgentner@ucol.mx>
2022-03-16 22:55:39 -0600

    ✅ Practica 10 - Final Commit with documentation

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import express from 'express';
 // Declaration of app constant and express function is assigned to app const.
 const app = new express();
 
+// This variable assigns the port that express will be using.
 let port = process.env.PORT || 3000;
 
+// Sending information to the server and route assignation (HTML).
 app.get('/', (req, res) => {
     res.send('<!DOCTYPE html><head></head><body>Hello World!</body></html>');
 });
 
+// This function assigns the port that express will be using, port parameter was assigned in port variable.
 app.listen(port);
 
+// Sending information to the server and route assignation (JSON).
 app.get('/api', (req, res) => {
     res.json({ firstname: 'John', lastname: 'Doe'});
 });
 
+// Sending information to the server and route assignation (HTML with REQUEST INFO (HTTP ROUTE)).
 app.get('/person/:id', (req, res) => {
     res.send(`<!DOCTYPE html><head></head><body><h1>Persona solicitada: ${req.params.id} </h1></body></html>`);
 });
