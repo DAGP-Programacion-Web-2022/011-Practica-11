54aec5a77d7aafa7ca3361661a3459a46e5adba2
dagp-ucol <dgentner@ucol.mx>
2022-03-16 22:44:54 -0600

    Practica 10 - res.json

--- src/index.js
+++ src/index.js
@@ -11,20 +11,6 @@ app.get('/', (req, res) => {
 
 app.listen(port);
 
-
-
-
-// // Sending information to the server and route assignation.
-
-// // Listen() function assigns the local host port that is being used by express.
-// app.listen(3000);
-
-// // Sending information to the server and route assignation.
-// app.get('/uno', (req, res) => {
-//     res.send('Hello World, this is the uno route.')
-// });
-
-// // Sending information to the server and route assignation.
-// app.get('/prueba', (req, res) => {
-//     res.send('Hello World, this is the prueba route.')
-// });
+app.get('/api', (req, res) => {
+    res.json({ firstname: 'John', lastname: 'Doe'});
+});
